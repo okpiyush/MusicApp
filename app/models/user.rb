cd44b16e13531fc037563b7f02e51d4a3bb30093
Piyush Kumar <piyush.kumar@gocomet.com>
2024-01-02 18:34:10 +0530

    validations added in the  commit

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  has_many :songs
+  validates :name, presence: true # validating if the name is present or not
+  validates :email, presence:true # validating if the email is present or not
+end
